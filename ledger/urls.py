c45006eb03f905cbd80e4a7523fc25ef4c93148a
coni1027 <nikolabitoria@gmail.com>
2025-02-13 22:57:53 +0800

    Added new functions in views.py and new paths in urls.py. Added templates directory project and appwide

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('', views.index, name='index'),
+    path('recipes/list', views.recipes_list, name='recipes_list'),
+    path('recipe/1', views.recipe1, name='recipe1'),
+    path('recipe/2', views.recipe2, name='recipe2')
 ]
 
 app_name = 'ledger'
