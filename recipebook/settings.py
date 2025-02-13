170e730b6c0470fc8f73253403e3dc345883923b
coni1027 <nikolabitoria@gmail.com>
2025-02-13 23:33:16 +0800

    Finished recipes.html template that reads from views.recipes_list

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'recipebook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
