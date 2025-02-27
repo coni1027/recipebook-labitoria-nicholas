60b4ac2b1f0be3cd66928bda8cb6dbbf661bdd45
coni1027 <nikolabitoria@gmail.com>
2025-02-27 20:22:31 +0800

    Initial commit of lab2, created three models for Ingredient, Recipe, and RecipeIngredient

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=100)
+    ingredient_field = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='recipe'
+    )
+    recipe_field = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='ingredient'
+    )
