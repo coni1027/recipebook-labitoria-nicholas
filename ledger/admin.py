bfbe038ac20e650a0c7b85baf4f483babe6dba78
coni1027 <nikolabitoria@gmail.com>
2025-02-27 22:43:00 +0800

    Fixed app-wide template, created admin code and added Queries

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Recipe, RecipeIngredient, Ingredient
 
 # Register your models here.
+class RecipeIngredientLine(admin.TabularInline):
+    model = RecipeIngredient
+
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    inlines = [RecipeIngredientLine]
+
+
+admin.site.register(Recipe, RecipeIngredientAdmin)
