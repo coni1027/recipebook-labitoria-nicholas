72a70d4d772ff89d1aa2b3a15bbab0c021240aca
coni1027 <nikolabitoria@gmail.com>
2025-03-24 20:29:55 +0800

    Modified detail page to show image, and added recipes_form html for adding recipes

--- ledger/views.py
+++ ledger/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from . import models
+from . import forms
 
 # Create your views here.
 
@@ -18,3 +20,10 @@ class RecipeListView(ListView):
 class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = models.Recipe
     template_name = 'ledger/recipes_view.html'
+
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = models.Recipe
+    form_class = forms.RecipeForm
+    template_name = 'ledger/recipes_form.html'
+    success_url = reverse_lazy('ledger:recipes_list')
