bc42d00770893304be83e84ccaf9dc2c9a1263f1
coni1027 <nikolabitoria@gmail.com>
2025-03-24 22:09:35 +0800

    Added views for adding images in recipe and modified htmls

--- ledger/urls.py
+++ ledger/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('recipes/list', views.RecipeListView.as_view(), name='recipes_list'),
     path('recipe/<int:pk>', views.RecipeDetailView.as_view(), name='recipe_detail'),
     path('recipe/add', views.RecipeCreateView.as_view(), name='recipe_add'),
+    path('recipe/<int:pk>/add_image', views.RecipeImageCreateView.as_view(), name='recipe_add_image')
 ]
 
 app_name = 'ledger'
--- ledger/views.py
+++ ledger/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from . import models
@@ -27,3 +28,24 @@ class RecipeCreateView(LoginRequiredMixin, CreateView):
     form_class = forms.RecipeForm
     template_name = 'ledger/recipes_form.html'
     success_url = reverse_lazy('ledger:recipes_list')
+
+
+class RecipeImageCreateView(LoginRequiredMixin, CreateView):
+    model = models.RecipeImage
+    form_class = forms.RecipeImageForm
+    template_name = 'ledger/recipes_add_image.html'
+
+    def form_valid(self, form):
+        recipe = get_object_or_404(models.Recipe, pk=self.kwargs['pk'])
+        recipe_image = form.save(commit=False)
+        recipe_image.recipe = recipe
+        recipe_image.save()
+        return redirect('ledger:recipe_detail', pk=recipe.pk)
+
+    def get_success_url(self):
+        return reverse_lazy('ledger:recipe_detail', kwargs={'pk': self.kwargs['pk']})
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recipe'] = get_object_or_404(models.Recipe, pk=self.kwargs['pk'])
+        return context
