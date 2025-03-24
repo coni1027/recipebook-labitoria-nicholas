b6519031dff2dbd8409ddb4438d661cb6fc01bff
coni1027 <nikolabitoria@gmail.com>
2025-03-24 15:26:47 +0800

    Initial commit, modified settings and added RecipeImage model

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,7 +124,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR/'static',
+]
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
+
 LOGIN_REDIRECT_URL = "/recipes/list"
 LOGOUT_REDIRECT_URL = "/accounts/login"
 
@@ -130,3 +139,5 @@ LOGOUT_REDIRECT_URL = "/accounts/login"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
